75607f2d3800ec20fca1b2d2dea7a84a7f4ec11b
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-01-06 22:09:30 -0300

    testing db in memory

--- testes/connection.spec.js
+++ testes/connection.spec.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const { expect } = require('chai');
 
-const models = {
-  users: [],
+const DBSERVER = new MongoMemoryServer();
+
+const getConnection = async () => {
+  const URLMock = await DBSERVER.getUri();
+  const OPTIONS = {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+  };
+  return MongoClient.connect(URLMock, OPTIONS);
 };
 
 describe('1 - Testando a conexão na rota raiz: "/"', () => {
+  before(async () => {
+    connectionMock = await getConnection();
+    sinon.stub(mockClient, 'connect').resolves(connectionMock);
+  });
+
+  after(async () => {
+    await connectionMock.db('class').collection('users').drop();
+    mongoClient.connect.restore();
+  });
+
   it('A rota deve retornar um array.', async () => {
     const response = await models.users;
     expect(response).to.be.a('array');
