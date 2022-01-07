b17c5b62ac88396cfa32fdb529bc423cdebf1050
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-01-06 22:42:18 -0300

    The controller was created to list all users.

---
+++ controllers/users.controllers.js
@@ -0,0 +1,13 @@
+const STATUS_OK = 200;
+
+const usersList = async (req, res, next) => {
+  try {
+    const users = await findAllUsers();
+    return res.status(STATUS_OK).json(users);
+  } catch (error) {
+    console.error(error.message);
+    return next(error);
+  }
+};
+
+module.exports = { usersList };
